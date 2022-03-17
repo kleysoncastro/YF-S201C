734a2c1849829c637fd04ed25dc671299b24a56a
klesyon castro <kleysoncastro99@gmail.com>
2022-03-17 13:59:14 -0300

    teste de envio do valor em fuber faito com sucesso

--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,30 @@
-#include <Arduino.h>
-#include <SPI.h>
-#define PIN3_INT_1 3
-#define SLAVE 10
+#include<SPI.h>
+char buf = 0;
 
-void incpulso1();
-
-
-float vazao1;                                              // declara variáveis para leitura das vazões
-int contaPulso1;                                           // declara as funções para contagem dos pulsos de cada rotâmetro
-
-void setup() {
-
-  digitalWrite(SLAVE, HIGH);
-  SPI.begin();
-
-
-  Serial.begin(9600);                         // habilita serial 0 e sua taxa de trabalho
-  pinMode(PIN3_INT_1, INPUT);                          // configura pino 3 para receber sinal e chamar interrupção 1
-  pinMode(21, INPUT);                         // configura pino 21 para receber sinal e chamar interrupção 2
-
-  attachInterrupt(1, incpulso1, RISING);      // habilita chamada à primeira interrupção (rotâmetro 1)
-
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(SS, INPUT_PULLUP);
+  pinMode(MOSI, OUTPUT);
+  pinMode(SCK, INPUT);
+  SPCR |= _BV(SPE);
+  SPI.attachInterrupt();  //allows SPI interrupt
 }
 
-void loop() {
+void loop(void)
+{
+
+    Serial.print("buf vindo do SPI: ");
   
-  contaPulso1 = 0;               // zera os contadores de pulsos
-  sei();          // habilita interrupção
-  delay(1000);                                    // aguarda 1 segundo entre leituras
-  cli();          // desabilita interrupção
+    Serial.println(buf, HEX);
+    Serial.println(buf);
 
-  vazao1 = contaPulso1 / 10.7;                     // equação para conversão da contagem de pulsos em fluxo (L/min) para rot. 1
-                                                  // espera-se que os fluxos não passem de 500 L/min
-  Serial.println(vazao1);                         // envia fluxo 1 para receptor MEGA
 
-  digitalWrite(SLAVE, LOW);
-  SPI.transfer(vazao1);
-  delay(100);
+    Serial.println("=============================================");
+  }
 
-}
-// rotina chamada pela interrupção provocada pelo rotâmetro 1
-void incpulso1() {
-  contaPulso1++;        // incrementa variável contando os pulsos do sensor 1
+
+ISR (SPI_STC_vect)   //Inerrrput routine function
+{
+  buf = SPDR;
 }
